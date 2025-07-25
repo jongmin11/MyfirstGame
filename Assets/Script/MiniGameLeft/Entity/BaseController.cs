09829f43c687b26a8768cec429398bc1fa0cfe7d
jongmin11 <gkswhdals01@gmail.com>
2025-07-25 20:28:11 +0900

    탑다운 게임 백업

--- Assets/Script/MiniGameLeft/Entity/BaseController.cs
+++ Assets/Script/MiniGameLeft/Entity/BaseController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    protected Rigidbody2D _rigidbody2D;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private SpriteRenderer chracterRenderer;
+    [SerializeField] private Transform wenponPivot;
+    
+    protected Vector2 movementDirection = Vector2.zero; 
+
+    public Vector2 MovementDirection { get { return movementDirection; } }
+    
+    protected Vector2 lookDirection = Vector2.zero;
+    
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 konckback = Vector2.zero;
+    private float knockbackDuretion = 0f;
+
+    protected virtual void Awake()
     {
-        
+
     }
 }
