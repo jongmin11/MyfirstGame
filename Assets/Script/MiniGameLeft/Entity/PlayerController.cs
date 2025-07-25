04cb49385ab93bb27f84c6148b2c862f9d158c43
jongmin11 <gkswhdals01@gmail.com>
2025-07-25 21:01:38 +0900

    백업2

--- Assets/Script/MiniGameLeft/Entity/PlayerController.cs
+++ Assets/Script/MiniGameLeft/Entity/PlayerController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Camera camera;
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+        camera = Camera.main;
     }
 }
